7ec8ef9049e8835aa932d24802033d815a33a0b6
Isaac <embramaze@gmail.com>
2019-12-04 19:09:32 +1100

    Walking/Jumping/StartingMazeSetup

---
+++ TickingBomb/Assets/Scripts/PlayerCameraMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class PlayerCameraMovement : MonoBehaviour
+{
+
+    //Creating different variables for the different Axis.
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    public float sensX = 15f;
+    public float sensY = 15f;
+
+    public float minX = -360f;
+    public float maxX = 360f;
+
+    public float minY = -60f;
+    public float maxY = 60f;
+
+    private float rotationY = 0f;
+
+    public bool canMove;
+
+    //Calling upon the Rigidbody, and freezing their rotations.
+    void Start()
+    {
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+
+        canMove = true;
+    }
+
+    void Update()
+    {
+
+        if(canMove)
+        {
+            if (axes == RotationAxes.MouseXAndY)
+            {
+                float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensX;
+
+                rotationY += Input.GetAxis("Mouse Y") * sensY;
+                rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+                transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+            }
+            else if (axes == RotationAxes.MouseX)
+            {
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensX, 0);
+            }
+            else
+            {
+                rotationY += Input.GetAxis("Mouse Y") * sensY;
+                rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+                transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+            }
+        }
+    }
+}
---
+++ TickingBomb/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    CharacterController controller;
+    public float verticalVelocity;
+    public float gravity = 14.0f;
+    public float currentjumpForce = 10.0f;
+    public float currentSpeed = 1.0f;
+
+    //Calling on the CharacterController Component
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    //Calling the PlayerJumping function
+    void Update()
+    {
+        PlayerJumping();
+    }
+
+    //Creating the player jumping, and player movement function.
+    //If the player is on ground then he is able to jump, depending on the jumpforce and gravity.
+    void PlayerJumping()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = currentjumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * currentSpeed);
+    }
+}
