04a22eb9c0ec59f4cba1508177c2b89e091a793d
Isaac <embramaze@gmail.com>
2019-12-06 12:33:59 +1100

    Timer/Panels/Maze/Button/Wires

---
+++ TickingBomb/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float timer;
+
+    public Text timeText;
+
+    public PlayerCameraMovement PCMX, PCMY;
+
+    public GameObject youWin;
+    public GameObject youLost;
+
+    public GameObject inGameHUD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+
+        timer = 30f;
+
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timer <= 5)
+        {
+            timeText.text = "Time Left: " + timer.ToString("F2");
+        }
+        else
+        {
+            timeText.text = "Time Left: " + timer.ToString("F0");
+        }
+
+        if (timer <= 0)
+        {
+            timer = 0f;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
---
+++ TickingBomb/Assets/Scripts/Interaction.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class Interaction : MonoBehaviour
+{
+    private bool visible;
+
+    public GameObject rayStart;
+
+    public GameObject connectedWires;
+    public GameObject wires;
+    public bool isWireConnected;
+    public GameObject button;
+
+    private void Start()
+    {
+        isWireConnected = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RayCastActivator();
+        ConnectingWires();
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+        if (Physics.Raycast(rayStart.transform.position, rayStart.transform.forward, out hit, 100))
+        {
+            Debug.DrawLine(rayStart.transform.position, hit.point);
+        }
+    }
+
+    void RayCastActivator()
+    {
+
+        //raycast from pliers
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(rayStart.transform.position, rayStart.transform.forward, out hit, 100))
+            {
+                Debug.DrawLine(rayStart.transform.position, hit.point);
+                print(hit.collider.name);
+            }
+        }
+    }
+
+    void ConnectingWires()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(rayStart.transform.position, rayStart.transform.forward, out hit, 100))
+            {
+                print(hit.collider.name);
+
+                if (hit.collider.CompareTag("Wires"))
+                {
+                    isWireConnected = true;
+                    connectedWires.SetActive(true);
+                    wires.SetActive(false);
+                }
+
+                if (hit.collider.CompareTag("Button") && isWireConnected == false)
+                {
+
+                    Debug.Log("Wire Not Connected");
+                }
+            }
+
+            Debug.DrawLine(ray.origin, hit.point);
+        }
+    }
+}
